3b6aace10cf3e9a867f66595750bcf39e3b4f438
LothbrockBjorn <82374353+LothbrockBjorn@users.noreply.github.com>
2021-11-21 14:10:53 +0700

    Update conn.php

--- _config/conn.php
+++ _config/conn.php
@@ -1,10 +1,10 @@
 <?php
 
 $sql_details = array(
-    'user' => 'root',
-    'pass' => '',
-    'db'   => 'dbasekpv2',// perlu yang database v2
-    'host' => 'localhost'
+    'user' => 'b4ee75cba9cbc3',
+    'pass' => 'bc8484ff',
+    'db'   => 'heroku_a169840466e5521',// perlu yang database v2
+    'host' => 'us-cdbr-east-04.cleardb.com'
 );
 
 $con = $sql_details;
